c7e510edc3825ce871a74fdc29f0bad6f4cf64ae
Elke Heymans <el.heymans@gmail.com>
2020-04-08 21:51:41 +0200

    Testing out vue-remark for Markdown blogposts

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,20 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'ElkeDotCodes',
-  plugins: []
-}
+  siteName: "ElkeDotCodes",
+  plugins: [
+    {
+      use: "@gridsome/vue-remark",
+      options: {
+        typeName: "BlogPost",
+        baseDir: "./content/blogposts",
+        template: "./src/templates/BlogPost.vue",
+        route: "/blog/:slug",
+      },
+    },
+  ],
+};
---
+++ src/templates/BlogPost.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+    <h1>{{ $page.blogPost.title }}</h1>
+    <p class="intro">{{ $page.blogPost.excerpt }}</p>
+    <VueRemarkContent />
+  </Layout>
+</template>
+
+<page-query>
+query BlogPost ($id: ID!) {
+  blogPost(id: $id) {
+    title
+    excerpt
+  }
+}
+</page-query>
