50ebf299b7a4808da60e6396f2617b83b5fbae1c
Elke Heymans <el.heymans@gmail.com>
2020-04-18 08:38:49 +0200

    [ADA] Added test coverage thresholds

--- ada/jest.config.js
+++ ada/jest.config.js
@@ -9,5 +9,13 @@ module.exports = {
     "!**/**.config.{ts,js}",
     "!**/{main,lib}.ts"
   ],
-  coverageReporters: ["html", "text-summary"]
+  coverageReporters: ["html", "text-summary"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10
+    }
+  }
 };
