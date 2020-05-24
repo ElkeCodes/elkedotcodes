af209d913cbf01dc4f2857fa0b15401c9a2e386b
Elke Heymans <el.heymans@gmail.com>
2020-05-24 09:29:42 +0200

    [SITE] Styling of the logo

--- site/src/layouts/Default.vue
+++ site/src/layouts/Default.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="container max-w-full md:max-w-screen-md mx-auto p-4 md:p-8">
     <header class="full-width flex justify-between h-16 mb-8">
-      <g-link to="/" class="font-bold text-4xl flex content-center">{{
-        $static.metadata.siteName
-      }}</g-link>
+      <g-link to="/" class="w-screen md:w-auto text-center font-bold text-4xl">Elke<span class="text-primary text-7xl leading-6">.</span>codes</g-link>
       <nav class="hidden md:flex">
         <g-link class="self-center ml-4 px-4 py-2 rounded-full transition duration-300 ease-in-out transform hover:-translate-y-1 hover:bg-primary" to="/">Home</g-link>
         <g-link class="self-center ml-4 px-4 py-2 rounded-full transition duration-300 ease-in-out transform hover:-translate-y-1 hover:bg-primary" to="/about/">About me</g-link>
