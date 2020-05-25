0881b16f4bf8769768cd5e579d42181137aa85eb
Elke Heymans <el.heymans@gmail.com>
2020-05-25 08:15:36 +0200

    [ADA] [SITE] Updated colour scheme and implemented hamburger menu quite literally

--- site/src/layouts/Default.vue
+++ site/src/layouts/Default.vue
@@ -1,20 +1,66 @@
 <template>
-  <div class="container max-w-full md:max-w-screen-md mx-auto p-4 md:p-8">
-    <header class="full-width flex justify-between h-16 mb-8">
-      <g-link to="/" class="w-screen md:w-auto text-center font-bold text-4xl">Elke<span class="text-primary text-7xl leading-6">.</span>codes</g-link>
-      <nav class="hidden md:flex">
-        <g-link class="self-center ml-4 px-4 py-2 rounded-full transition duration-300 ease-in-out transform hover:-translate-y-1 hover:bg-primary" to="/">Home</g-link>
-        <g-link class="self-center ml-4 px-4 py-2 rounded-full transition duration-300 ease-in-out transform hover:-translate-y-1 hover:bg-primary" to="/about/">About me</g-link>
+  <div class="container max-w-full md:max-w-screen-md mx-auto">
+    <header class="full-width flex items-baseline justify-between mb-2">
+      <g-link to="/" class="w-screen md:w-auto text-center font-bold text-4xl md:ml-8 mt-4 md:mt-8 py-2"
+        >Elke<span class="text-primary text-7xl leading-6">.</span>codes</g-link
+      >
+      <nav class="hidden md:flex mr-8 mt-8">
+        <g-link
+          v-for="menuItem in menuItems"
+          :key="menuItem.label"
+          class="self-center ml-4 px-4 py-2 font-black rounded-full transition duration-300 ease-in-out transform hover:-translate-y-1 hover:bg-primary"
+          :to="menuItem.to"
+          >{{ menuItem.label }}</g-link
+        >
+      </nav>
+      <div @click="isMenuOpen = true" class="absolute right-0 text-2xl px-4 py-2 md:hidden">🍔</div>
+      <nav
+        class="flex fixed w-screen h-screen overflow-hidden bg-primary flex-col transition-max-h duration-700 ease-in-out"
+        :class="{
+          'max-h-0': !isMenuOpen,
+          'max-h-screen': isMenuOpen,
+        }"
+      >
+        <button class="text-right text-2xl px-4 py-2 mb-4" @click="isMenuOpen = false">🙅‍♀️</button>
+        <g-link
+          v-for="menuItem in menuItems"
+          :key="menuItem.label"
+          class="text-center font-black text-2xl p-4"
+          @click="isMenuOpen = false"
+          :to="menuItem.to"
+          >{{ menuItem.label }}</g-link
+        >
       </nav>
     </header>
-    <div class="full-width">
+    <div class="full-width p-4 md:p-8">
       <slot />
     </div>
-    <footer class="full-width mt-16 pt-2 border-t text-tiny">
-      &copy; 2020 Elke Heymans - Made with Vue, Gridsome, TailwindCSS, GraphQL and ☕️
+    <footer class="full-width m-4 md:m-8 border-t text-tiny">
+      &copy; 2020 Elke Heymans - Made with Vue, Gridsome, TailwindCSS, GraphQL
+      and ☕️
     </footer>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isMenuOpen: false,
+      menuItems: [
+        {
+          to: "/",
+          label: "Home"
+        },
+        {
+          to: "/about",
+          label: "About me"
+        }
+      ]
+    };
+  }
+};
+</script>
 
 <static-query>
 query {
